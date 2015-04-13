7ffed08f8a4a688826fc04f47c166b12bc1dbc4c
Roderick Hsiao <roderickhsiao@gmail.com>
2015-04-12 22:43:21 -0700

    Adding ES6 example

---
+++ javascript/basic/inheritanceClass.js
@@ -0,0 +1,35 @@
+/* jshint node:true */
+'use strict';
+
+class Animal {
+    constructor(name) {
+        this.legs = null;
+        this.name = name;
+        this.tails = null;
+    }
+
+    saySomething() {
+        return 'Say something';
+    }
+
+    eatSomething() {
+        return 'I am eating';
+    }
+}
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+        this.tails = 1;
+    }
+
+    saySomething() {
+        return 'Bark';
+    }
+}
+
+module.exports = {
+    Animal: Animal,
+    Dog: Dog
+};
