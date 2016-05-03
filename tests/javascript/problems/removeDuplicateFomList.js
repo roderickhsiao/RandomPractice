3522dca51bb43271db5eda08efc9884e8bbe7ed4
Roderick Hsiao <roderickhsiao@gmail.com>
2016-05-02 21:57:45 -0700

    remove unused function

--- tests/javascript/problems/removeDuplicateFomList.js
+++ tests/javascript/problems/removeDuplicateFomList.js
@@ -2,9 +2,6 @@ import {expect} from 'chai';
 
 import deleteDuplicates from '../../../javascript/problems/removeDuplicateFomList';
 
-var createNode = (arr) => {
-    return
-};
 describe('#deleteDuplicates', () => {
     it('delete duplicate number from list', () => {
         var spec = { val: 1, next: { val: 1, next: null } };
