68be408fb77ef37e917257bab0f4e6444d940315
Roderick Hsiao <roderickhsiao@gmail.com>
2016-05-22 23:56:37 -0700

    Remove only test

--- tests/javascript/algorithms/luhn.js
+++ tests/javascript/algorithms/luhn.js
@@ -6,7 +6,7 @@ import isValid from '../../../javascript/algorithms/luhn';
 
 describe('#Bubble Sort', () => {
     describe('Positive test cases', () => {
-        it.only('should validate correct number', () => {
+        it('should validate correct number', () => {
             expect(isValid(79927398710)).to.equal(false);
             expect(isValid(79927398713)).to.equal(true);
         });
